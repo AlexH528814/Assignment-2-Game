139400f3a4f6e6c45008bff4f2546ce0c52d33aa
AlexH528814 <528814@escg.ac.uk>
2022-10-20 19:07:39 +0100

    Up to ep 4 of tut

--- Assignment 2 Game/Assets/Scripts/PlayerMovement.cs
+++ Assignment 2 Game/Assets/Scripts/PlayerMovement.cs
@@ -5,30 +5,64 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
 
-    [SerializeField] private float speed= 5f;
+    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jump = 5f;
 
-    float dirX;
+    private float dirX = 0;
+
+    private enum MovementState { idle, running, jumping}
+    private MovementState state = MovementState.idle;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
-        
-        rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
+
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jump);
         }
 
+        UpdateAnimState();
+    }
 
+    private void UpdateAnimState()
+    {
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+
+        else if(rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+
+        else
+        {
+            state = MovementState.idle;
+        }      
+
+        anim.SetInteger("state", (int)state);
     }
 }
